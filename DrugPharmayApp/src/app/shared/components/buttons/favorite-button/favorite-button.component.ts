ef4844285e45ab076d1e774ff4c7d809686924d6
Grosse Riese <plmman2010@gmail.com>
2023-08-12 10:33:15 +0300

    Create favorite-button.component.ts

---
+++ DrugPharmayApp/src/app/shared/components/buttons/favorite-button/favorite-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-button',
+  templateUrl: './favorite-button.component.html',
+  styleUrls: ['./favorite-button.component.css']
+})
+export class FavoriteButtonComponent {
+
+}
