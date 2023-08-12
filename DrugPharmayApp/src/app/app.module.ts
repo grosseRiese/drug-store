81aff904284751670d8318f96a6f66cf75cfafa6
Grosse Riese <plmman2010@gmail.com>
2023-08-12 10:32:44 +0300

    Update app.module.ts

--- DrugPharmayApp/src/app/app.module.ts
+++ DrugPharmayApp/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { FavoriteButtonComponent } from './shared/components/buttons/favorite-button/favorite-button.component';
+import { CollapseButtonComponent } from './shared/components/buttons/collapse-button/collapse-button.component';
+import { AuthDirective } from './shared/directives/auth.directive';
+import { CapitalizePipe } from './shared/pipes/capitalize.pipe';
+import { SafePipe } from './shared/pipes/safe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    LoaderComponent,
+    FavoriteButtonComponent,
+    CollapseButtonComponent,
+    AuthDirective,
+    CapitalizePipe,
+    SafePipe
   ],
   imports: [
     BrowserModule,
