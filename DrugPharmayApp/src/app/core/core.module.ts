cd5439eaf1fecbb7a7da79475c6bb893308e4329
Grosse Riese <plmman2010@gmail.com>
2023-08-12 11:04:41 +0300

    Update core.module.ts

--- DrugPharmayApp/src/app/core/core.module.ts
+++ DrugPharmayApp/src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer/footer.component';
-import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
