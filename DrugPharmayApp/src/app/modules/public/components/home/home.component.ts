8ac19bfd1030652d8dbe6c3aae5911f3150838d8
Grosse Riese <plmman2010@gmail.com>
2023-08-14 08:52:55 +0300

    Update home.component.ts

--- DrugPharmayApp/src/app/modules/public/components/home/home.component.ts
+++ DrugPharmayApp/src/app/modules/public/components/home/home.component.ts
@@ -11,10 +11,31 @@ import { DrugService } from '../../services/drug.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
+  public drugs: IDrug[] = [];
+  //selectedDrug: any;
+  selectedDrugForm!: FormGroup;
 
-  constructor() {}
+  constructor(private drugService: DrugService, private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.selectedDrugForm = new FormGroup({
+      selectedDrugName: new FormControl('', [Validators.required]),
+      selectedDrugQuantity: new FormControl(0, [Validators.required, Validators.min(1)]), } );
+
+    this.loadDrugsList();
+  }
+
+  loadDrugsList():void {
+    this.drugService.getDrugs().subscribe({
+      next:(data) => {
+
+        this.drugs = data;
+        console.log("Drugs: ", this.drugs);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
   }
 
 }
