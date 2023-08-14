722c846ab53d24d20c37a9cafcd8f62a8ec3fe4d
Grosse Riese <plmman2010@gmail.com>
2023-08-14 07:57:18 +0300

    Update app-routing.module.ts

--- DrugPharmayApp/src/app/app-routing.module.ts
+++ DrugPharmayApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/public/public-routing.module').then((m) => m.PublicRoutingModule),
+  },
+
+  { path: '**', redirectTo: 'error/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
