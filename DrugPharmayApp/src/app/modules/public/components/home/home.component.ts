3ce1864587769fb70126160e9509ac4330145e1f
Grosse Riese <plmman2010@gmail.com>
2023-08-15 19:28:00 +0300

    Update home.component.ts

--- DrugPharmayApp/src/app/modules/public/components/home/home.component.ts
+++ DrugPharmayApp/src/app/modules/public/components/home/home.component.ts
@@ -15,32 +15,13 @@ export class HomeComponent implements OnInit{
   public drugs: IDrug[] = [];
   selectedDrugForm!: FormGroup;
 
-
-  countries: any[] | undefined;
-  selectedCountry: string | undefined;
-
   constructor(private drugService: DrugService, private cartService: CartService) {}
 
   ngOnInit(): void {
-
-    this.countries = [
-      { name: 'Australia', code: 'AU' },
-      { name: 'Brazil', code: 'BR' },
-      { name: 'China', code: 'CN' },
-      { name: 'Egypt', code: 'EG' },
-      { name: 'France', code: 'FR' },
-      { name: 'Germany', code: 'DE' },
-      { name: 'India', code: 'IN' },
-      { name: 'Japan', code: 'JP' },
-      { name: 'Spain', code: 'ES' },
-      { name: 'United States', code: 'US' }
-  ];
-
-
-
     this.selectedDrugForm = new FormGroup({
       selectedDrugName: new FormControl('', [Validators.required]),
       selectedDrugQuantity: new FormControl(0, [Validators.required, Validators.min(1)]), } );
+      
     this.loadDrugsList();
   }
 
