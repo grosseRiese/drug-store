9a789a91174e6c5b838bdf1415de1372dd663fb6
Grosse Riese <plmman2010@gmail.com>
2023-08-15 10:46:01 +0300

    Update app-routing.module.ts

--- DrugPharmayApp/src/app/app-routing.module.ts
+++ DrugPharmayApp/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./modules/public/public-routing.module').then((m) => m.PublicRoutingModule),
+      import('./modules/public/public.module').then((m) => m.PublicModule),
   },
 
   { path: '**', redirectTo: 'error/404' },
