e9074415e6baf5866d526b1fe38b40449083d278
Grosse Riese <plmman2010@gmail.com>
2023-08-16 17:47:08 +0300

    Update order.service.ts

--- DrugPharmayApp/src/app/modules/public/services/order.service.ts
+++ DrugPharmayApp/src/app/modules/public/services/order.service.ts
@@ -1,22 +1,62 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { DrugService } from './drug.service';
+import { IDrug } from '../models/idrug';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  private availableDrugs: BehaviorSubject<{ [drugName: string]: number }> = new BehaviorSubject({});
 
-  constructor() {}
+  private inputSignal$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-  // Update the available drugs quantity
-  updateAvailableDrugs(drugs: { [drugName: string]: number }): void {
-    this.availableDrugs.next(drugs);
+  private inputQuantitySignal$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  drugs: IDrug[] = this.drugService.drugs;
+  
+  constructor(private drugService:DrugService) {
+
+     // Subscribe to input changes and update button availability
+    this.inputSignal$.subscribe(drugName => {
+      const desiredQuantity = this.inputQuantitySignal$.value;
+      this.updateAddButtonAvailability(drugName, desiredQuantity);
+    });
+
+    this.inputQuantitySignal$.subscribe(quantity => {
+      const drugName = this.inputSignal$.value;
+      this.updateAddButtonAvailability(drugName, quantity);
+    });
+
   }
 
-  // Check if a drug quantity is available
+   // Function to update the drug name input
+  updateDrugName(drugName: string) {
+    this.inputSignal$.next(drugName);
+  }
+
+  // Function to update the quantity input
+  updateQuantity(quantity: number) {
+    this.inputQuantitySignal$.next(quantity);
+  }
+
+  // Function to update 'ADD' button availability based on drug name and quantity
+  updateAddButtonAvailability(drugName: string, quantity: number) {
+    const drug = this.drugs.find(d => d.name === drugName);
+    if (drug && quantity <= drug.quantity) {
+      console.log(`'ADD' button is available for ${drugName}`);
+    } else {
+      console.log(`'ADD' button is not available for ${drugName}`);
+    }
+  }
+   // Check if a drug quantity is available
   isDrugAvailable(drugName: string, quantity: number): boolean {
-    const availableDrugs = this.availableDrugs.value;
-    return availableDrugs[drugName] >= quantity;
+    const drug = this.drugs.find(d => d.name === drugName);
+
+    if (drug && quantity !=0) {
+      return quantity <= drug.quantity; // Check if requested quantity is less than or equal to available quantity
+    }
+  
+    return false; // Drug not found
   }
+
 }
