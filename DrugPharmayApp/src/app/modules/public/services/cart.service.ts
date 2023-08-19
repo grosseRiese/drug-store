d4fd7a58b6d7136d4d1701c29a89105cb42e0097
Grosse Riese <plmman2010@gmail.com>
2023-08-19 09:35:38 +0300

    Update cart.service.ts

--- DrugPharmayApp/src/app/modules/public/services/cart.service.ts
+++ DrugPharmayApp/src/app/modules/public/services/cart.service.ts
@@ -2,21 +2,21 @@ import { Injectable, computed, signal } from '@angular/core';
 import { IDrug } from '../models/idrug';
 import { DrugService } from './drug.service';
 import { BehaviorSubject } from 'rxjs';
-import { CartItem } from '../models/cart-item';
+import { ICartItem } from '../models/cart-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cartItems$: BehaviorSubject<CartItem[]> = new BehaviorSubject<CartItem[]>([]);
+  private cartItems$: BehaviorSubject<ICartItem[]> = new BehaviorSubject<ICartItem[]>([]);
 
   constructor(private drugService:DrugService) { }
 
-  getCartItems(): BehaviorSubject<CartItem[]> {
+  getCartItems(): BehaviorSubject<ICartItem[]> {
     return this.cartItems$;
   }
 
-  addToCart(cartItem: CartItem): void {
+  addToCart(cartItem: ICartItem): void {
     const currentCartItems = this.cartItems$.value;
     this.cartItems$.next([...currentCartItems, cartItem]);
   }
