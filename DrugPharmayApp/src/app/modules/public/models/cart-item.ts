0324ebeb82702882bdbe8721165ae7a9e822d3be
Grosse Riese <plmman2010@gmail.com>
2023-08-19 09:35:36 +0300

    Update cart-item.ts

--- DrugPharmayApp/src/app/modules/public/models/cart-item.ts
+++ DrugPharmayApp/src/app/modules/public/models/cart-item.ts
@@ -1,8 +1,9 @@
-export interface CartItem {
+export interface ICartItem {
   drugName: string;
   quantity: number;
   price: number;
   id: number;
   isAvailable: boolean;
   totalPrice:number;
+  expireDate: Date;
 }
