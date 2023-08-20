13faf247144e8385907883bbc535fb857fd723c5
Grosse Riese <plmman2010@gmail.com>
2023-08-20 14:01:26 +0300

    Update cart.component.ts

--- DrugPharmayApp/src/app/modules/public/components/cart/cart.component.ts
+++ DrugPharmayApp/src/app/modules/public/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ICartItem } from '../../models/cart-item';
 import { OrderService } from '../../services/order.service';
@@ -6,11 +6,13 @@ import { OrderService } from '../../services/order.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+
 })
 export class CartComponent {
   cartItems: Observable<ICartItem[]> = this.orderService.getCartItems();
 
   constructor( public orderService:OrderService) {}
 
+  
 }
