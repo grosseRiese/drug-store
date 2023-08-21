14ec668b615be53e86ec894b2d907d66498a4feb
Grosse Riese <plmman2010@gmail.com>
2023-08-21 12:24:39 +0300

    Update cart.component.ts

--- DrugPharmayApp/src/app/modules/public/components/cart/cart.component.ts
+++ DrugPharmayApp/src/app/modules/public/components/cart/cart.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ICartItem } from '../../models/cart-item';
 import { OrderService } from '../../services/order.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -13,14 +14,30 @@ export class CartComponent {
   cartItems$: Observable<ICartItem[]> = this.orderService.getCartItems();
   totalSum: number = 0; // Initialize the total sum
 
-  constructor( public orderService:OrderService) {
+  ////////////////////////////////////////////////////////////////
+  private drugNameChange$: BehaviorSubject<{ originalName: string; newName: string }> = new BehaviorSubject<{ originalName: string; newName: string }>({ originalName: '', newName: '' });
+
+  
+  constructor( public orderService:OrderService,
+    public cartService:CartService) {
     //this.cartItems$ = this.orderService.getCartItems(); 
 
     this.cartItems$.subscribe((items) => {
       this.totalSum = items.reduce((sum, item) => sum + item.totalPrice, 0);
     });
-
- 
-
+    
   }
+
+
+  onDrugNameChange(item: ICartItem): void {
+    const originalDrugName = item.drugName;
+    const newDrugName = item.drugName; // Assuming the drug name is updated in the template
+
+    this.cartService.sendDrugNameChange(originalDrugName, newDrugName);
+  }
+
+  getDrugNameChangeObservable(): Observable<{ originalName: string; newName: string }> {
+    return this.drugNameChange$.asObservable();
+  }
+
 }
